ff1ad740137eb49ff205dc4105c57dd1a8327de5
s2018 <s2018s35@e-mirim.hs.kr>
2020-06-02 10:21:20 +0900

    초기 커밋.

---
+++ overridemethos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overridemethos
+{
+    class Program
+    {
+        /*static int Test(int A)
+        {
+            return 10;
+        }*/
+        /*static int Test(long A)
+        {
+            return 20;
+        }*/
+        /*static int Test(double A)
+        {
+            return 40;
+        }*/
+
+        //1. static int Power(int input)
+        //{
+        //    return input * input;
+        //}
+        //2. static int Power(int input, int count)
+        //{
+        //    return count=input *2;
+        //}
+        //3.static int SumAll(int end)
+        //{
+        //    for (int i = 0; i <= end; i++)
+        //        int sum+= end;
+        //    return sum;
+        //}
+        //4. static int SumAll(int start, int end)
+        //{
+        //    int sum=start += end;
+        //    return sum;
+        //}
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Test(52273));
+            //Console.WriteLine(Test(52.273));
+
+        }
+    }
+}
